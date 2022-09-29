66e03eb21fa9704c82e04d490274030abebd727d
zibby-zibs <53227231+zibby-zibs@users.noreply.github.com>
2022-09-29 13:29:11 +0100

    updated link to the server

--- src/Create.js
+++ src/Create.js
@@ -11,7 +11,7 @@ const Create = () => {
     e.preventDefault();
     const blog = { title, body, author };
 
-    fetch('https://fake-serv-blog.herokuapp.com/', {
+    fetch('https://fake-serv-blog.herokuapp.com/blogs', {
       method: 'POST',
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog)
